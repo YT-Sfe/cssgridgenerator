c9ccdae0307fc405fe5a76d6c0672f58d09fea81
zyt <zhangyatao@jizhousoft.com>
2021-06-04 14:17:59 +0800

    发布

--- src/utils/repetition.spec.js
+++
@@ -1,9 +0,0 @@
-import {groupRepeatedUnits} from "./repetition";
-
-describe("Repetition", () => {
-    it("groupRepeatedUnits", () => {
-        const result = groupRepeatedUnits();
-        
-        expect(result).toEqual([["1fr"]]);
-    })
-});
